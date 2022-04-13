e90d40e6d0b108eb33812bee8b0a9c6d2016fdbf
Jawerro <svobodnaypochta@gmail.com>
2022-04-13 17:24:16 +0300

    My django app, second commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from importlib.resources import path
+from unittest.mock import patch
+from django.urls import URLPattern, path
+from . import views
+
+urlpatterns = [
+    path('',views.post_list, name='post_list')
+]
